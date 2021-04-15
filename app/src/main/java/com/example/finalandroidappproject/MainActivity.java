404c8e22c0d6174cf2ac84a2b92ec327282d9ecb
Blueglass2 <78095712+Blueglass2@users.noreply.github.com>
2021-04-15 08:50:56 -0500

    fixes
    1231

--- app/src/main/java/com/example/finalandroidappproject/MainActivity.java
+++ app/src/main/java/com/example/finalandroidappproject/MainActivity.java
@@ -3,33 +3,37 @@ package com.example.finalandroidappproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
 
 
 //Referenced this to find the auth
 // https://www.androidhive.info/2016/06/android-getting-started-firebase-simple-login-registration-auth/
 public class MainActivity extends AppCompatActivity {
     // reference to entire database
-    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "EmailPassword";
+
     private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
     }
 
-    @Override
+
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -52,35 +56,37 @@ public class MainActivity extends AppCompatActivity {
 
     //https://firebase.google.com/docs/auth/android/password-auth?authuser=0
     public void signUp(View v) {
+
         EditText emailEditText = (EditText) findViewById(R.id.textEmailAddress);
         EditText passEditText = (EditText) findViewById(R.id.textPassword);
 
         String email = emailEditText.getText().toString();
         String password = passEditText.getText().toString();
+
         Log.e("KIM", "Typing " + email + " and " + password);
 
-        //Successful -> .then code
-        //Unsuccessful -> catch code (catch error message)
-
-        auth.createUserWithEmailAndPassword(email, password).
-                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Log.e("KIM", "createUserWithEmail:success");
+                            Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = auth.getCurrentUser();
-                            //updateUI(user);
+
                         } else {
                             // If sign in fails, display a message to the user.
-                            Log.e("KIM", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
+
                         }
                     }
                 });
+
     }
+
+
 
     /*public void signIn(View v) {
         EditText emailEditText = (EditText) findViewById(R.id.textEmailAddress);
@@ -99,6 +105,21 @@ public class MainActivity extends AppCompatActivity {
 
         }).catch(e => Log.e(e.message));
     }*/
+
+    /**
+     * This method will be called to minimize the on screen keyboard in the Activity
+     * When we get the current view, it is the view that has focus, which is the keyboard
+     * Credit - Found and suggested by Ram Dixit, 2019
+     *
+     * Source:  https://www.youtube.com/watch?v=CW5Xekqfx3I
+     */
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();     // view will refer to the keyboard
+        if (view != null ){                     // if there is a view that has focus
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
 }
 
 
