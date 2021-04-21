5f9f42e879dec53febe9b9008ed579fee44295ff
Blueglass2 <78095712+Blueglass2@users.noreply.github.com>
2021-04-21 09:01:06 -0500

    Added addTaskActivity
    asfss

---
+++ app/src/main/java/com/example/finalandroidappproject/AddTasksActivity.java
@@ -0,0 +1,56 @@
+package com.example.finalandroidappproject;
+
+-import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class AddTasksActivity extends AppCompatActivity {
+    private static final String TAG = "taskadd";
+
+    private FirebaseFirestore db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tasks);
+    }
+
+    public void addTask (View v) {
+        EditText taskNameEditText = (EditText) findViewById(R.id.taskNameText);
+
+        String taskName = taskNameEditText.getText().toString();
+
+        db.collection("events")
+                .add(taskName)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        //toastMessage("Event stored successfully");
+                        Log.i(TAG, "Success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //toastMessage("Event failed to add");
+                        Log.i(TAG, "Failure");
+                    }
+                });
+    }
+
+}
+
