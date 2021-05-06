54cb2fa801d5132b01554e6e8058a78a0b795b10
Blueglass2 <78095712+Blueglass2@users.noreply.github.com>
2021-05-06 14:02:11 -0500

    got rid of errors in DisplayTasksActivity
    -See title

--- app/src/main/java/com/example/finalandroidappproject/DisplayTasksActivity.java
+++ app/src/main/java/com/example/finalandroidappproject/DisplayTasksActivity.java
@@ -57,45 +57,42 @@ public class DisplayTasksActivity extends AppCompatActivity {
         tasksListView.setAdapter(customAdapter);
 
 
-<<<<<<< HEAD
-=======
-        //allTasksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            //@Override
-            /**public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
->>>>>>> NewCal
-
-        // Create a setOnItemClickListener for the listView to find out which element they clicked on
         tasksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View v, int i, long l) {
-                TaskParcelable taskO = myTasks.get(i);
-<<<<<<< HEAD
-                Log.i(TAG, taskO.getKey());
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                key = taskO.getKey();
-                /*
-                // start an intent to load the page to edit this element that has been clicked on
-                Intent intent = new Intent(DisplayTasksActivity.this, DisplayTaskDetailsActivity.class);
-                intent.putExtra("taskParc", taskO);
-                startActivity(intent);*/
+
+                // Create a setOnItemClickListener for the listView to find out which element they clicked on
+                tasksListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View v, int i, long l) {
+                        TaskParcelable taskO = myTasks.get(i);
+
+                        Log.i(TAG, taskO.getKey());
+
+                        key = taskO.getKey();
+                        /*start an intent to load the page to edit this element that has been clicked on
+                        Not going to use maybe?
+                        Intent intent = new Intent(DisplayTasksActivity.this, DisplayTaskDetailsActivity.class);
+                        intent.putExtra("taskParc", taskO);
+                        startActivity(intent);*/
+                    }
+                });
+
             }
-        });
-
-=======
-                Log.i(TAG, taskO.toString());
-
-            }*/
 
             /**This function removes the task that's selected from the firebase firestore
              *
              * @param v
              */
-            //public void removeTask(View v, int i){
-                //db.collection("TaskList").document(myTasks.get(i).getKey()).delete();
-            //}
-       // });
->>>>>>> NewCal
+            public void removeTask(View v, int i){
+                db.collection("TaskList").document(myTasks.get(i).getKey()).delete();
+            }
+
+       });
     }
+
+
 
     /**This function sends the user to the activity where you can add tasks
      *
@@ -131,7 +128,7 @@ public class DisplayTasksActivity extends AppCompatActivity {
         }
     }
 
-    /**private void showData() {
+    private void showData() {
         ArrayList<TaskParcelable> myTasks = new ArrayList<TaskParcelable>();
         db.collection("TaskList").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -153,7 +150,7 @@ public class DisplayTasksActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }*/
+    }
 
 
     class CustomAdapter extends BaseAdapter {
@@ -199,20 +196,19 @@ public class DisplayTasksActivity extends AppCompatActivity {
             TextView taskNameTV = (TextView) view.findViewById(R.id.taskName);
 
             // Here I am getting the specific element in the database we are currently displaying
-<<<<<<< HEAD
+
             TaskParcelable t = myTasks.get(i);
 
             // Set the correct image, event name, and event date for the Event object we are
             // displaying in the list
             taskNameTV.setText(t.getTaskName());
 
-=======
-            //TaskParcelable e = myTasks.get(i);
+
+            TaskParcelable e = myTasks.get(i);
 
             // Set the correct image, event name, and event date for the Event object we are
             // displaying in the list
-            //taskNameTV.setText(e.getTaskName());
->>>>>>> NewCal
+            taskNameTV.setText(e.getTaskName());
 
             // return this view element with the correct data inserted
             return view;
