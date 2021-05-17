c954425317ea941587f2c57278c778989990f4d8
Blueglass2 <78095712+Blueglass2@users.noreply.github.com>
2021-05-16 21:51:32 -0500

    Added date
    dfa

--- app/src/main/java/com/example/finalandroidappproject/TaskParcelable.java
+++ app/src/main/java/com/example/finalandroidappproject/TaskParcelable.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 public class TaskParcelable implements Parcelable {
     private String taskName;
+    private String taskDate;
     private String key;
 
     /**
@@ -45,25 +46,30 @@ public class TaskParcelable implements Parcelable {
      */
     public TaskParcelable(Parcel parcel){
         taskName = parcel.readString();
+        taskDate = parcel.readString();
         key = parcel.readString();
     }
 
     /**Constructor
      *
-     * @param taskName
+     * @param taskName name of the task
+     * @param taskDate the time of the task
      */
-    public TaskParcelable(String taskName){
+    public TaskParcelable(String taskName, String taskDate){
         this.taskName = taskName;
+        this.taskDate = taskDate;
         this.key = "no key yet";
     }
 
     /**Constructor
      *
-     * @param taskName
-     * @param key
+     * @param taskName name of the task
+     * @param taskDate the time of the task
+     * @param key id of the task
      */
-    public TaskParcelable(String taskName, String key){
+    public TaskParcelable(String taskName, String taskDate, String key){
         this.taskName = taskName;
+        this.taskDate = taskDate;
         this.key = key;
     }
 
@@ -77,6 +83,7 @@ public class TaskParcelable implements Parcelable {
      */
     public void writeToParcel(Parcel dest, int flags){
         dest.writeString(taskName);
+        dest.writeString(taskDate);
         dest.writeString(key);
     }
 
@@ -102,5 +109,8 @@ public class TaskParcelable implements Parcelable {
         return taskName;
     }
 
+    public String getTaskDate(){
+        return taskDate;
+    }
 
 }
